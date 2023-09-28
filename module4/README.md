57d77a7d7a0f5c286006c036ed56b56d5ec5b2b8
Dmitrij Mironov <mironouz@gmail.com>
2023-09-28 21:33:19 +0200

    add sql for tables creation

--- module4/README.md
+++ module4/README.md
@@ -4,6 +4,8 @@
 
 ## Design
 
-Let's design the simple database with 3 tables: students, subjects and exam_results.
+Let's design the simple database with 3 tables: students, subjects and exam_results. Students and Subjects tables both has id as the primary key which is foreign key in Exam results table and demonstrates one-to-many relationship, so each result can be identified by participating of single student in single subject. 
 
 ![alt text](design.png)
+
+Let's create physical model of the database in postgress. SQL for tables creation can be checked in [create_db.sql](create_db.sql)
---
+++ module4/create_db.sql
@@ -0,0 +1,28 @@
+drop table if exists students cascade;
+
+create table students (
+	student_id serial primary key,
+    name varchar(20) not null,
+    surname varchar(20) not null,
+	date_of_birth date not null,
+	phone_number text check (char_length(phone_number) = 9),
+	primary_skill varchar(50),
+	created_datetime timestamp not null,
+	updated_datetime timestamp not null
+);
+
+drop table if exists subjects cascade;
+
+create table subjects (
+	subject_id serial primary key,
+    subject_name varchar(20) unique not null,
+    tutor varchar(20) not null
+);
+
+drop table if exists exam_results;
+
+create table exam_results (
+	student_id int references students(student_id),
+	subject_id int references subjects(subject_id),
+	mark int not null check (mark between 1 and 5)
+);
